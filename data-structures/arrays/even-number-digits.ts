9e99308a507948d9373e97ffd7e467044bec1e81
MrDanii <dan333_d@hotmail.com>
2024-11-08 22:22:26 -0600

    Folder fix

--- data-structures/arrays/even-number-digits.ts
+++
@@ -1,9 +0,0 @@
-function findNumbers(nums: number[]): number {
-  let evenQty: number = 0
-  for (const num of nums) {
-    let numberOfDigits: number = (num+"").length
-    if ((numberOfDigits & 1) === 0) evenQty++
-  }
-
-  return evenQty
-};
