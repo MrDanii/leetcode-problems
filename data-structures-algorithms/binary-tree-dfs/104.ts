c63fdc96be83b8c8c367c0544e4cd392b1e284da
MrDanii <dan333_d@hotmail.com>
2024-11-19 20:05:41 -0600

    Leetcode 75, Binary-tree DFS completed, (104, 872)

---
+++ data-structures-algorithms/binary-tree-dfs/104.ts
@@ -0,0 +1,45 @@
+
+// Definition for a binary tree node.
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+
+
+function maxDepth(root: TreeNode | null): number {
+  if (root == null) return 0
+  console.log(root.left);
+  console.log(root.right);
+  let leftNodeDepth: number = 1 + maxDepth(root.left)
+  let rightNodeDepth: number = 1 + maxDepth(root.right)
+  console.log(`left (${leftNodeDepth}) -- right (${rightNodeDepth})`);
+  return (leftNodeDepth >= rightNodeDepth) ? leftNodeDepth : rightNodeDepth;
+};
+
+// Example #1:
+//      3
+//   9      20
+//       15   7
+
+const treeNode5: TreeNode | null = new TreeNode(7, null, null)
+const treeNode4: TreeNode | null = new TreeNode(15, null, null)
+const treeNode3: TreeNode | null = new TreeNode(20, treeNode4, treeNode5)
+const treeNode2: TreeNode | null = new TreeNode(9, null, null)
+const treeNode1: TreeNode | null = new TreeNode(3, treeNode2, treeNode3)
+
+console.log(maxDepth(treeNode1));
+
+// Example #2:
+        //       0
+        //   1       2       
+        // 3   4    5  6
+
+// PreOrder (root, left, right): [0,1,3,4,2,5,6]
+// InOrder (left, root, right): [3,1,4,0,5,2,6]
+// PostOrder (left, right, root): [3,4,5,6,1,2,0]
---
+++ data-structures-algorithms/binary-tree-dfs/872.ts
@@ -0,0 +1,71 @@
+// Definition for a binary tree node.
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+
+
+function leafSimilar(root1: TreeNode | null, root2: TreeNode | null): boolean {
+  let arrRoot1: number[] = []
+  let arrRoot2: number[] = []
+  
+  getLeaves(root1, arrRoot1)
+  getLeaves(root2, arrRoot2)
+
+  if (arrRoot1.length != arrRoot2.length) return false  
+  let isLeafSimilar: boolean = true
+  // comparing each element of leafs from tree #1 with leafs from tree #2
+  for (let i = 0; i < arrRoot1.length; i++){
+    if (arrRoot1[i] != arrRoot2[i]) {
+      isLeafSimilar = false
+      break
+    }
+  }
+   
+  return isLeafSimilar
+};
+
+function getLeaves(root: TreeNode | null, arr: number[]) {
+  if (root == null) return
+  console.log(root.val);
+  if (root.left == null && root.right == null) {
+    arr.push(root.val)
+    return
+  }
+  getLeaves(root.left, arr)
+  getLeaves(root.right, arr)
+}
+
+const treeNode1: TreeNode | null = new TreeNode(3)
+treeNode1.left = new TreeNode(5)
+treeNode1.right = new TreeNode(1)
+treeNode1.left.left = new TreeNode(6)
+treeNode1.left.right = new TreeNode(2)
+treeNode1.left.right.left = new TreeNode(7)
+treeNode1.left.right.right = new TreeNode(4)
+treeNode1.right.left = new TreeNode(9)
+treeNode1.right.right = new TreeNode(8)
+
+// const auxArr1: number[] = []
+// // console.log(getLeaves(treeNode1, auxArr1));
+// // console.log(auxArr1);
+
+const treeNode2: TreeNode | null = new TreeNode(3)
+treeNode2.left = new TreeNode(5)
+treeNode2.right = new TreeNode(1)
+treeNode2.left.left = new TreeNode(6)
+treeNode2.left.right = new TreeNode(7)
+treeNode2.right.left = new TreeNode(4)
+treeNode2.right.right = new TreeNode(2)
+treeNode2.right.right.left = new TreeNode(9)
+treeNode2.right.right.right = new TreeNode(8)
+
+// console.log(treeNode2);
+
+console.log(leafSimilar(treeNode1, treeNode2));
